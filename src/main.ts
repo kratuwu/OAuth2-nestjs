f00099d65f5960da73bceb8f46e1f2b8d8128132
Krittawat S <kratuwu@gmail.com>
2022-02-25 17:59:10 +0700

    fix local cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: configService.get('FRONTEND_URL'),
+    credentials: true,
+  });
+  await app.listen(8000);
 }
 bootstrap();
